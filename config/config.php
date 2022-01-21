03ec088a2b638b5a07407bae2557b13b5cd4b9c5
Manoj M <manoj904378@gmail.com>
2022-01-21 14:03:31 +0530

    configuration added

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('HOST_URL','http://localhost:8086/'); 
+define('BASE_PATH', realpath(dirname(__FILE__)));
+
+// Database configuration
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'product_table');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+//Time-Zone for Database
+define('TIMEZONE','Asia/kolkata');
